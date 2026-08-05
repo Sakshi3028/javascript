9d28321243039c7755f990396dbdaf115f9a79b5
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-08-05 18:18:49 +0530

    initial commit

---
+++ variables.js
@@ -0,0 +1,10 @@
+const accountId=1234567890
+let accountEmail="sakshi@gmail.com"
+var accountPassword="sakshi@123"
+let accountCity="delhi"
+
+accountEmail="123@gmail.com"
+accountPassword="123@456"
+accountCity="mumbai"
+console.log(accountId);
+console.table({accountEmail,accountPassword,accountCity})   
