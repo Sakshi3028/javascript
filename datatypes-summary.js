0e2b410834cc1ca90d2434d825f54c403c79f2bb
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-08-06 20:52:47 +0530

    datatypes

---
+++ datatypes-summary.js
@@ -0,0 +1,21 @@
+//primitive data types
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//non-primitive data types
+// 1 type: Object
+// 2 type: arrya
+// 3 type: functions
+
+//bigint is used to store large numbers
+// const bigInt = 1234567890123456789012345678901234567890n;
+
+//curlybraces are used to define objects
+
+let myFunction = function() {
+    console.log("hello");
+}
+console.log(typeof myFunction);
+
+// primitive dataypes me (stack) use hoti h aur non-primitive me (heap) use hoti h
+
+
