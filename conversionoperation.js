45595defcb031b951ab4f331e74c9cb1c7f323bc
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-08-06 20:13:32 +0530

    conversion of datatypes

---
+++ conversionoperation.js
@@ -0,0 +1,11 @@
+let score="33"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+let somenumber=33
+let Stringnumber=String(somenumber)
+console.log( Stringnumber);
+console.log(typeof Stringnumber);
