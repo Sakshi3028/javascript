ef3e3740300aeaa867e65a996b7068879afd3fdb
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-08-07 21:07:40 +0530

    array

---
+++ array.js
@@ -0,0 +1,33 @@
+const myarr=[1,2,3,4,5]
+// console.log(myarr);
+// console.log(myarr[0]);
+// console.log(myarr.length);
+
+//array methods
+// console.log(myarr.pop(3));
+// console.log(myarr);
+
+// myarr.push(0);
+// console.log(myarr);
+
+// const newarr=myarr.join()
+// console.log(myarr);
+// console.log(newarr);
+// console.log(typeof myarr);
+// console.log(typeof newarr);
+
+//slice,splice
+// console.log("A",myarr);
+// const myn1 = myarr.slice(1,3)
+// console.log("B",myn1);
+// console.log("B",myarr);
+// const myn2=myarr.splice(1,3)
+// console.log("C",myarr);
+// console.log(myn2);
+
+
+const another_array=[1,2,3,[4,5,6],7,[8,9,[1,2]]]
+const reat_another_array=another_array.flat(2)
+console.log(reat_another_array);
+
+
---
+++ dates.js
@@ -0,0 +1,7 @@
+let myDate=new Date();
+console.log(myDate);
+console.log(myDate.getDay());
+console.log(typeof myDate);
+console.log(myDate.toDateString());
+newdate=myDate.toDateString();
+console.log(newdate);
