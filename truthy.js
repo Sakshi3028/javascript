990cfa6a0f77b8fbce26e29fe19271aec98dee4c
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-08-10 22:55:26 +0530

    commit

---
+++ truthy.js
@@ -0,0 +1,13 @@
+const userEmail = "sakshi@gmail.com"
+
+if(userEmail){
+    console.log("got user email");
+}else{
+    console.log("Dont have user emial");
+}
+
+//falsy value
+// false,0,-0,BigInt, 0n,"",null,undefined,Nan
+
+//truthy value
+//"0",'false'," ",[],{},funstion(){}
