8ebe7a38fe4f91fcf2c820e594e1d40dce5c1678
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-08-10 17:44:31 +0530

    commit

---
+++ arrow.js
@@ -0,0 +1,22 @@
+const user={
+    username:"sakshi",
+    price: 999,
+
+    welcomemessage: function(){
+        console.log(`$ {username} welcome to website`);
+
+    }
+}
+
+// const chai = () => {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// curly braces me return krte h bracket me nhi krte h
+
+const addtwo= (num1,num2) => (num1 +num2)
+
+console.log(addtwo(3,4))
