05319f0e0b2ed250a02efd9ed62b90dd08d514be
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-08-10 22:49:07 +0530

    commit

--- condition.js
+++ condition.js
@@ -1,3 +1,32 @@
 if(2==2){
     console.log("true")
 }
+
+// format
+
+// switch(key) {
+//     case value:
+//         break;
+//         default:
+//             break;
+// }
+
+const month=3
+switch(month){
+    case 1:
+        console.log("january");
+        break;
+         case 2:
+        console.log("feb");
+        break;
+         case 3:
+        console.log("march");
+        break;
+         case 4:
+        console.log("april");
+        break;
+
+        default:
+            console.log("default case match")
+            break;
+}
