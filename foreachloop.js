55fa5a8e2ad0e3b2cdb0b37ab3d00d2768ef66c0
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-08-11 17:55:03 +0530

    commit

--- foreachloop.js
+++ foreachloop.js
@@ -1,7 +1,12 @@
 
 //for each loop
-const coding=["js","ruby","py","cpp","html"]
-coding.forEach(function(val) {
-    console.log(val);
-})
+// const coding=["js","ruby","py","cpp","html"]
+// coding.forEach(function(val) {
+//     console.log(val);
+// })
 
+const mynums=[1,2,3,4,5,6,7,8,9]
+const newnums=mynums.filter((num)=>{
+     return num>5}
+)
+console.log(newnums);
