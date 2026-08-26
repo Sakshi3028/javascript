42cc48f4bf2744e1f5b9227e985730066286c94e
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-08-26 17:21:56 +0530

    commit

--- API/object.js
+++ API/object.js
@@ -1,4 +1,5 @@
 function multiplyBy5(num){
+    this.num=num
     return num* 5
 }
 multiplyBy5.power = 2
@@ -6,3 +7,21 @@ multiplyBy5.power = 2
 console.log(multiplyBy5(5));
 console.log(multiplyBy5.power);
 console.log(multiplyBy5.prototype);
+
+function createUser(username,score){
+    this.username = username
+    this.score=score
+}
+createUser.prototype.increment = function(){
+    this.score ++
+}
+
+createUser.prototype.printMe = function(){
+    console.log(`score is ${this.score}`)
+}
+const chai=  new createUser("chai",25)
+const tea = createUser("tea",250)
+
+// console.log(this.tea);
+chai.printMe()
+
